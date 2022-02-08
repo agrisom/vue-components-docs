bfe296910ae402b26c6c59e94fd0178d7dedf1b0
agrisom <albert.griso.mendez@gmail.com>
2022-02-08 19:54:20 +0100

    Adds vue-component-ui dependency

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,22 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
+  <h1>Using components from vue-components-ui</h1>
+  <span>version: @0.0.1</span>
   <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <UILabel text="Hello world from UILabel"/>
   </p>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { UILabel } from 'vue-components-ui';
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
+export default defineComponent({
+  name: 'HelloWorld',
+  components: {
+    UILabel,
+  },
+});
+</script>
 
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
+<style>
 </style>
